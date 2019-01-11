addb53382fe30a0025d086bf4c5b89e7acf8a855
ran88dom99 <rain8dome9@gmail.com>
2019-01-11 15:54:25 -0500

    cause effect

--- cause_effect_statistics.R
+++ cause_effect_statistics.R
@@ -21,8 +21,8 @@ bad.state<-c(  "s-fart"     ,   "s-dizzy"     ,    "s-headache" ,   "s-nauseous"
                "s-compulsion" , "s-lung"  ) #unique(dt.state$state1c)
 good.state<-c("s-excited","s-energetic")
 dt.test[,cause:=FALSE];dt.state[,cause:=TRUE];dt.food[,cause:=TRUE];dt.drug[,cause:=TRUE];dt.act[,cause:=TRUE];
-dt.test[,Effect_of_interest:=TRUE];dt.state[,Effect_of_interest:=TRUE];dt.food[,Effect_of_interest:=FALSE];dt.drug[,Effect_of_interest:=FALSE];
-dt.act[,Effect_of_interest:=first_level_of_event %in% bad.activites];
+dt.test[,Effect_of_interest:=TRUE];dt.state[,Effect_of_interest:=F];dt.food[,Effect_of_interest:=FALSE];dt.drug[,Effect_of_interest:=FALSE];
+dt.act[,Effect_of_interest:= F] #first_level_of_event %in% bad.activites];
 
 #dt.recombined<-dt.test[, .(inears,location.long,location,location.short,time,event,first_level_of_event,impact,category,Effect_of_interest,cause)] 
 dt.recombined<-rbindlist(list(dt.test[, .(inears,location.long,location,location.short,time,event,first_level_of_event,impact,category,Effect_of_interest,cause)],
@@ -47,6 +47,10 @@ effects.unique <- as.vector(unique(dt.recombined[Effect_of_interest==TRUE , .(fi
 window.lengths <- c(.5,1,10,30,60,180,600,1200,6000,15000) * minute.constant
 window.lengths <- round(window.lengths, digits = 5)
 compare.window <- 3
+df.record <- data.frame(
+  highest.effect=1, cause=1, cause.count=1, effect=1, effect.count=1, window=1,
+  incubation=1, comparison.count=1)
+(df.record <- df.record[FALSE, ])
 
 for (cause in causes.unique) {
   for (effect in effects.unique) {
@@ -74,9 +78,10 @@ for (cause in causes.unique) {
   
         }
         cause.count<-sum(dt.worked$cause)
-        effect.count<-sum(dt.worked$effect)
+        effect.count<-sum(dt.worked[inzone == T]$effect)
         dt.worked<-dt.worked[inzone == T & effect == T]
         comparison.count <- sum(dt.worked$impacted==0)
+        if(effect.count<6) next()
         
         try({
         corr<-cor.test( dt.worked$impact, dt.worked$impacted,
@@ -93,6 +98,10 @@ for (cause in causes.unique) {
         }
         highest.effect<-round(highest.effect, digits = 3)
         
+        df.record<-rbind(df.record,
+                         data.frame(highest.effect , cause, cause.count, 
+                                    effect, effect.count, window,
+                                    incubation, comparison.count))
         print(paste(
           highest.effect , cause, cause.count, effect, effect.count, window,
           incubation, comparison.count
@@ -104,4 +113,71 @@ for (cause in causes.unique) {
   }
 }
 
+if(F)
+{
+  df.record<-data.table(df.record)
+  df.record[,abs.hi.cor:=abs(highest.effect)] 
+  str(df.record)
+  df.rework<-data.table(df.record[(!str_detect(df.record$effect, "-")),])
+  df.rework[,abs.hi.cor:=abs(highest.effect)]
 
+  
+  cor.test(df.rework$abs.hi.cor,df.rework$window) #25% at 1/20 2-sided
+  cor.test(df.rework$abs.hi.cor,df.rework$incubation)
+  cor.test(df.rework$abs.hi.cor,df.rework$cause.count)
+  cor.test(df.rework$abs.hi.cor,df.rework$effect.count)#-7%
+  cor.test(df.rework$abs.hi.cor,df.rework$comparison.count) #-11%
+  
+  df.rework2<-df.rework[df.rework$effect %in% c("p","VR","m20lc","m19lc")]
+  
+  
+  
+  #record.entry<- df.rework2[2501,];datasource<-dt.recombined
+  drawrecord <- function(record.entry,datasource)
+  {
+    cause<-record.entry$cause[1]; effect<-record.entry$effect[1]; 
+    window<-record.entry$window[1]; incubation<-record.entry$incubation[1];
+    
+    selection <- datasource$first_level_of_event==cause | datasource$first_level_of_event==effect
+    dt.worked <- datasource[selection]
+    dt.worked$cause <- dt.worked$first_level_of_event==cause
+    dt.worked$effect <- dt.worked$first_level_of_event==effect
+    
+    dt.worked[,impacted:=0]
+    dt.worked[,inzone:=FALSE]
+    for (each.cauz in which(dt.worked$cause)) {
+      
+      start.window <- dt.worked[each.cauz]$time + incubation
+      end.window <- dt.worked[each.cauz]$time + incubation + window
+      mpakt <- dt.worked[each.cauz]$impact
+      if( length( dt.worked[time > start.window & time < end.window]$impact ) >0)
+        dt.worked[time > start.window & time < end.window]$impacted <- dt.worked[time > start.window & time < end.window]$impacted + mpakt
+      
+      start.window <- dt.worked[each.cauz]$time + incubation -  window - compare.window
+      end.window <- dt.worked[each.cauz]$time + incubation + window + compare.window
+      if( length( dt.worked[time > start.window & time < end.window]$impact ) >0)
+        dt.worked[time > start.window & time < end.window]$inzone <- TRUE
+      
+    }
+    cause.count<-sum(dt.worked$cause)
+    effect.count<-sum(dt.worked[inzone == T]$effect) 
+  
+    #library(ggraptR)
+    #ggraptR()
+    
+    #see what the correlation (impact and impacted) is actualy based on then 
+    #timelines of both cause and effect
+    print(ggplot(dt.worked[inzone == T & effect==T], aes(y=impact, x=impacted)) + geom_point(stat="identity", position="jitter", alpha=0.5, size=3) + geom_smooth(stat="smooth", position="identity", method="auto", formula=y ~ x, se=TRUE, n=80, level=0.95, span=0.75) + facet_grid(first_level_of_event ~ .) + theme_grey() + theme(text=element_text(family="sans", face="plain", color="#000000", size=15, hjust=0.5, vjust=0.5)) + scale_size(range=c(1, 3)) + xlab("impacted") + ylab("impact")
+    )
+    print(ggplot(dt.worked[inzone == T & effect==T], aes(y=impact, x=time)) + geom_point(stat="identity", position="jitter", alpha=0.5, size=3) + geom_smooth(stat="smooth", position="identity", method="auto", formula=y ~ x, se=TRUE, n=80, level=0.95, span=0.75) + facet_grid(first_level_of_event ~ .) + theme_grey() + theme(text=element_text(family="sans", face="plain", color="#000000", size=15, hjust=0.5, vjust=0.5)) + scale_size(range=c(1, 3)) + xlab("time") + ylab("impact")
+    )
+    print(ggplot(dt.worked[inzone == T & effect==T], aes(y=impacted, x=time)) + geom_point(stat="identity", position="jitter", alpha=0.5, size=3) + geom_smooth(stat="smooth", position="identity", method="auto", formula=y ~ x, se=TRUE, n=80, level=0.95, span=0.75) + facet_grid(first_level_of_event ~ .) + theme_grey() + theme(text=element_text(family="sans", face="plain", color="#000000", size=15, hjust=0.5, vjust=0.5)) + scale_size(range=c(1, 3)) + xlab("time") + ylab("impacted")
+    )
+    print(ggplot(dt.worked[inzone == T & cause==T], aes(x = time, weights = impact)) + geom_density(adjust = 1/4)
+    )
+    #ggplot(dt.worked[inzone == T & cause==T], aes(x=time)) + geom_density(aes(y=..density..), stat="density", position="identity", alpha=0.5,adjust = 1/4) + facet_grid(first_level_of_event ~ .) + theme_grey() + theme(text=element_text(family="sans", face="plain", color="#000000", size=15, hjust=0.5, vjust=0.5)) + xlab("time") + ylab("density")
+    
+  }
+  drawrecord(df.rework2[4700,],dt.recombined)
+  
+}
